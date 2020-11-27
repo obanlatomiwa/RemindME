7eea5a15c7d27391cff49605c79c41c8a6e80735
Tomiwa Obanla <obanlatomiwa@gmail.com>
2020-11-28 00:39:53 +0100

    new changes

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <ul>
+      <remindList
+        v-for="resources in storedResources"
+        :key="resources.id"
+        :title="resources.title"
+        :description="resources.description"
+        :link="resources.link"
+      ></remindList>
+    </ul>
+  </div>
+</template>
+
+<script>
+import remindList from '@/components/resources/remindList.vue';
+export default {
+  components: {
+    remindList
+  },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official Vue.js documentation.',
+          link: 'https://vuejs.org'
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to google...',
+          link: 'https://google.org'
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style scoped></style>
